a0b84b631e4b8df977e2f4c05dd74600842a398c
barlasai <barlasai542@gmail.com>
2025-12-22 21:34:25 +0530

    Time: 14 ms (90.74%) | Memory: 13.3 MB (50.94%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        target = len(nums)-1
+
+        for i in range(len(nums) - 1, -1, -1):
+           
+            if i + nums[i] >= target:
+                target = i
+            
+
+       
+        if target == 0:
+            return True
+        else:
+            return False
