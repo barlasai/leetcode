e87595ebd2dfe902b7ea7088b1b66f79ca819aff
barlasai <barlasai542@gmail.com>
2026-02-15 23:31:01 +0530

    Time: 18 ms (60.53%) | Memory: 18.1 MB (73.50%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,31 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
     def mergeKLists(self, lists):
-        if not lists:
+        """
+        :type lists: List[Optional[ListNode]]
+        :rtype: Optional[ListNode]
+        """
+        if lists == [] or lists == None:
             return None
-        if len(lists) == 1:
-            return lists[0]
-        mid = len(lists) // 2
-        l, r = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
-        return self.merge(l, r)
-    
-    def merge(self, l, r):
-        dummy = p = ListNode()
-        while l and r:
-            if l.val < r.val:
-                p.next = l
-                l = l.next
-            else:
-                p.next = r
-                r = r.next
-            p = p.next
-        p.next = l or r
-        return dummy.next
-    
-    def merge1(self, l, r):
-        if not l or not r:
-            return l or r
-        if l.val< r.val:
-            l.next = self.merge(l.next, r)
-            return l
-        r.next = self.merge(l, r.next)
-        return r
+        else:
+            pq = []
+            for i in range(len(lists)):
+                if lists[i] != None:
+                    item = (lists[i].val, i, lists[i])
+                    heapq.heappush(pq, item)
+            dummy = ListNode(0)
+            p = dummy
+
+            while pq != []:
+                heap_item = heapq.heappop(pq)
+                p.next = heap_item[2]
+                p = p.next
+                if heap_item[2].next != None:
+                    item = (heap_item[2].next.val, heap_item[1], heap_item[2].next)
+                    heapq.heappush(pq, item)
+            return dummy.next
