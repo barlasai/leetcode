cdb6a67ed73388572e63ac17f15810cb91996c5a
barlasai <barlasai542@gmail.com>
2026-02-16 22:44:07 +0530

    Time: 22 ms (42.50%) | Memory: 13.8 MB (22.37%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = [-1]
+        max_len = 0
+
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if stack:
+                    max_len = max(max_len, i - stack[-1])
+                else:
+                    stack.append(i)
+
+        return max_len
