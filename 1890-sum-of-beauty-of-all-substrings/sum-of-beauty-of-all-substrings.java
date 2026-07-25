bbc0884e69a48b3214c2ea3c61d8ac68a3e9a0c9
BARLA VENKATA SIVA SAI <barlasai542@gmail.com>
2026-07-25 22:06:18 +0530

    Time: 63 ms (48.16%) | Memory: 44.2 MB (47.07%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,35 @@
+class Solution {    
+        /*
+        #########################################################################
+        #                                                                       #
+        #  =============================================                        #
+        #                  SIDDARDHA CHILUVERU                                  #
+        #  =============================================                        #
+        #                                                                       #
+        #  Author      : Siddardha Chiluveru                                    #
+        #  Description : Solution / Code / Project                              #
+        #  Date        : 2026-07-22                                             #
+        #                                                                       #
+        #########################################################################
+        */
+    public int beautySum(String s) {
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26]; 
+            for (int j = i; j < n; j++) {
+                char c = s.charAt(j);
+                freq[c - 'a'] += 1;
+                int min = 500;
+                int max = 0;
+                for (int num : freq) 
+                    if (num > 0) {
+                        min = Math.min(min, num);
+                        max = Math.max(max, num);
+                    }
+                ans += (max - min);
+            }
+        }
+        return ans;
+    }
+}
